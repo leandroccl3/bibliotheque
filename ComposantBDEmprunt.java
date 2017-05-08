c2e8c5dfbbb088c402c0e1a5c34086ec9095dcee
leandroccl3 <leandroccl3@hotmail.com>
2017-05-08 10:16:22 +0200

    Update ComposantBDEmprunt.java
    HAY ERRORES!!!

--- ComposantBDEmprunt.java
+++ ComposantBDEmprunt.java
@@ -19,28 +19,26 @@ public class ComposantBDEmprunt {
    * @return le nombre d'emprunts.
    * @throws SQLException en cas d'erreur de connexion à la base.
    */
-  public static int nbEmpruntsEnCours() throws SQLException {
-      public static int nbEmpruntsEnCours() throws SQLException {
-	  ArrayList<String[]> nbEmpCur = new ArrayList<String[]>();
-	  Statement stmt = Connexion.getConnection().createStatement();
-	  String sql="select idem from emprunts where dateret = ('1111-11-11') ";
-	  ResultSet rset = stmt.executeQuery(sql);
-	  
-	  
-	  
-	  while(rset.next()){
-		  String[] nbEmpCurs = new String[1];
-		  nbEmpCurs[0] = rset.getString("idem");
-		  nbEmpCur.add(nbEmpCurs);
-		  }
-	  
-	  rset.close();
-	  stmt.close();
-
-	  int sizeNbEmpCur = nbEmpCur.size();
-	
-	  return sizeNbEmpCur;
-  }
+	 public static int nbEmpruntsEnCours() throws SQLException {
+		 
+		 ArrayList<String[]> nbEmpCur = new ArrayList<String[]>();
+		 
+		 Statement stmt = Connexion.getConnection().createStatement();
+		 String sql="select idem from emprunts where dateret = ('1111-11-11') ";
+		 ResultSet rset = stmt.executeQuery(sql);
+		 
+		 while(rset.next()){
+			 String[] nbEmpCurs = new String[1];
+			 nbEmpCurs[0] = rset.getString("idem");
+			 nbEmpCur.add(nbEmpCurs);
+			 }
+		 
+		 rset.close();
+		 stmt.close();
+		 
+		 int sizeNbEmpCur = nbEmpCur.size();
+		 return sizeNbEmpCur;
+		 }
 
 
   /**
@@ -51,12 +49,25 @@ public class ComposantBDEmprunt {
    * @throws SQLException en cas d'erreur de connexion à la base.
    */
   public static int nbEmpruntsEnCours(int idAbonne) throws SQLException {
-    //
-    // A COMPLETER
-    //
-    return -1;
-  }
-
+	  
+	  ArrayList<String[]> nbEmpCurIdAbonne = new ArrayList<String[]>();
+		 
+		 Statement stmt = Connexion.getConnection().createStatement();
+		 String sql="select idem from emprunts where dateret = ('1111-11-11') and emprunteur = idAbonne ";
+		 ResultSet rset = stmt.executeQuery(sql);
+		 
+		 while(rset.next()){
+			 String[] nbEmpCurIdAbonnes = new String[1];
+			 nbEmpCurIdAbonnes[0] = rset.getString("idem");
+			 nbEmpCurIdAbonne.add(nbEmpCurIdAbonnes);
+			 }
+		 
+		 rset.close();
+		 stmt.close();
+		 
+		 int sizeNbEmpCurIdAbonne = nbEmpCurIdAbonne.size();
+		 return sizeNbEmpCurIdAbonne;
+		 }
   
   /**
    * Récupération de la liste complète des emprunts en cours.
@@ -77,13 +88,32 @@ public class ComposantBDEmprunt {
    * @throws SQLException en cas d'erreur de connexion à la base.
    */
   public static ArrayList<String[]> listeEmpruntsEnCours() throws SQLException {
-    ArrayList<String[]> emprunts = new ArrayList<String[]>();
-    //
-    // A COMPLETER
-    //
-    return emprunts;
-  }
-
+	  
+	  ArrayList<String[]> listeEmpruntsEnCours = new ArrayList<String[]>();
+		 
+		 Statement stmt = Connexion.getConnection().createStatement();
+		 String sql="select * from emprunts";
+		 ResultSet rset = stmt.executeQuery(sql);
+		 
+		 while(rset.next()){
+			 
+			 String[] listeEmpruntsEnCourss = new String[5];
+			 
+			 listeEmpruntsEnCourss[0] = rset.getString("idem");
+			 listeEmpruntsEnCourss[1] = rset.getString("dateemp");
+			 listeEmpruntsEnCourss[2] = rset.getString("dateret");
+			 listeEmpruntsEnCourss[3] = rset.getString("emprunteur");
+			 listeEmpruntsEnCourss[4] = rset.getString("exemempr");
+			 
+			 listeEmpruntsEnCours.add(listeEmpruntsEnCourss);
+			 
+			 }
+		 
+		 rset.close();
+		 stmt.close();
+		 
+		 return listeEmpruntsEnCours;
+		 }
   /**
    * Récupération de la liste des emprunts en cours pour un abonné donné.
    * 
@@ -100,11 +130,33 @@ public class ComposantBDEmprunt {
    * @throws SQLException en cas d'erreur de connexion à la base.
    */
   public static ArrayList<String[]> listeEmpruntsEnCours(int idAbonne) throws SQLException {
-    ArrayList<String[]> emprunts = new ArrayList<String[]>();
-    //
-    // A COMPLETER
-    //
-    return emprunts;
+    
+	ArrayList<String[]> empruntsEnCours = new ArrayList<String[]>();
+    
+	Statement stmt = Connexion.getConnection().createStatement();
+    String sql = "select ex.idex, liv.titre, us.nom,"
+    		+ " emp.dateemp, emp.dateret from usagers"
+    		+ " us join emprunts emp on us.idu=emp.emprunteur"
+    		+ " join exemplaire ex on ex.idex=emp.exemempr"
+    		+ " join livre liv on ex.exemliv=liv.id"
+    		+ " order by dateemp desc";
+    ResultSet rset = stmt.executeQuery(sql);
+
+    while (rset.next()) {
+      String[] listeEmpruntsEnCours = new String[5];
+      listeEmpruntsEnCours[0] = rset.getString("idex");
+      listeEmpruntsEnCours[1] = rset.getString("titre");
+      listeEmpruntsEnCours[2] = rset.getString("nom");
+      listeEmpruntsEnCours[3] = rset.getString("dateemp");
+      listeEmpruntsEnCours[4] = rset.getString("dateret");
+      
+      empruntsEnCours.add(listeEmpruntsEnCours);
+ 
+    }
+    rset.close();
+    stmt.close();
+    
+    return empruntsEnCours;
   }
 
   /**
@@ -128,12 +180,38 @@ public class ComposantBDEmprunt {
    * @throws SQLException en cas d'erreur de connexion à la base.
    */
   public static ArrayList<String[]> listeEmpruntsHistorique() throws SQLException {
-    ArrayList<String[]> emprunts = new ArrayList<String[]>();
-    //
-    // A COMPLETER
-    //
-    return emprunts;
+	    
+	ArrayList<String[]> listes = new ArrayList<String[]>();
+	        
+    Statement stmt = Connexion.getConnection().createStatement();
+    String sql = "select ex.idex, liv.titre, us.nom,"
+    		+ " emp.dateemp, emp.dateret from usagers"
+    		+ " us join emprunts emp on us.idu=emp.emprunteur"
+    		+ " join exemplaire ex on ex.idex=emp.exemempr"
+    		+ " join livre liv on ex.exemliv=liv.id"
+    		+ " order by dateemp desc";
+    ResultSet rset = stmt.executeQuery(sql);
+
+    while (rset.next()) {
+    	
+      String[] liste = new String[5];
+      
+      liste[0] = rset.getString("idex");
+      liste[1] = rset.getString("titre");
+      liste[2] = rset.getString("nom");
+      liste[3] = rset.getString("dateemp");
+      liste[4] = rset.getString("dateret");
+      
+      listes.add(liste);
+      
+      }
+    
+    rset.close();
+    stmt.close();
+    
+    return listes;
   }
+
 
   /**
    * Emprunter un exemplaire à partir de l'identifiant de l'abonné et de
